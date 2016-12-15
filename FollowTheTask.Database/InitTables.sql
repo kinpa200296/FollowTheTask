5e64ac3a880f6998e6363b76313105d97f61aab5
Pavel Kindruk <kinpa200296@yandex.ru>
2016-12-15 17:39:33 +0300

    Added SeedDb script for populating dummy data.

--- FollowTheTask.Database/InitTables.sql
+++ FollowTheTask.Database/InitTables.sql
@@ -27,7 +27,7 @@ INSERT INTO [dbo].[Statuses]([Name])
 
 SET IDENTITY_INSERT [dbo].[AuthData] ON
 INSERT INTO [dbo].[AuthData] ([Id], [PasswordHash], [Salt], [SecurityStamp], [LockoutEnabled], [AccessFailedCnt], [LockoutDateUtc], [AccessGrantedTotal], [LastAccessGrantedDateUtc], [AccessFailedTotal], [LastAccessFailedDateUtc])
-    VALUES (1, 'lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', '2487e0cb-fa63-43ff-b00e-a5ad13a22bc4', 0, 0, NULL, 0, NULL, 0, NULL);
+    VALUES (1, 'lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL);
 SET IDENTITY_INSERT [dbo].[AuthData] OFF
 
 SET IDENTITY_INSERT [dbo].[Users] ON
---
+++ FollowTheTask.Database/SeedDb.sql
@@ -0,0 +1,143 @@
+INSERT INTO [dbo].[AuthData] (PasswordHash, Salt, SecurityStamp, LockoutEnabled, AccessFailedCnt, LockoutDateUtc, AccessGrantedTotal, LastAccessGrantedDateUtc, AccessFailedTotal, LastAccessFailedDateUtc)
+    VALUES
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL),
+        ('lxUvyFwXdQt25i++waSfGhvmKWXIwigi7LP1hsdroI0=', 'sskwfPZT5E+FccQz+7II9A==', LOWER(NEWID()), 0, 0, NULL, 0, NULL, 0, NULL);
+
+INSERT INTO [dbo].[Users] (Username, Email, EmailConfirmed, FirstName, LastName, AuthId)
+    VALUES
+        ('kinpa', 'kinpa@kinpa.com', 1, 'Pavel', 'Kindruk', 2),
+        ('max0__o', 'max@max.com', 1, 'Max', 'Shlandakov', 3),
+        ('user1', 'user1@megauser.com', 1, 'User', '1', 4),
+        ('user2', 'user2@megauser.com', 1, 'User', '2', 5),
+        ('user3', 'user3@megauser.com', 1, 'User', '3', 6),
+        ('user4', 'user4@megauser.com', 1, 'User', '4', 7),
+        ('user5', 'user5@megauser.com', 1, 'User', '5', 8),
+        ('user6', 'user6@megauser.com', 1, 'User', '6', 9),
+        ('user7', 'user7@megauser.com', 1, 'User', '7', 10),
+        ('user8', 'user8@megauser.com', 1, 'User', '8', 11),
+        ('user9', 'user9@megauser.com', 1, 'User', '9', 12),
+        ('leader1', 'leader1@megaleader.com', 1, 'Leader', '1', 13),
+        ('leader2', 'leader2@megaleader.com', 1, 'Leader', '2', 14),
+        ('leader3', 'leader3@megaleader.com', 1, 'Leader', '3', 15),
+        ('guest1', 'guest1@megaguest.com', 1, 'Guest', '1', 16),
+        ('guest2', 'guest2@megaguest.com', 1, 'Guest', '2', 17),
+        ('guest3', 'guest3@megaguest.com', 1, 'Guest', '3', 18);
+
+INSERT INTO [dbo].[Teams](Name, LeaderId)
+    VALUES
+        ('FollowTheTask Team', 2),
+        ('Team 1', 13),
+        ('Team 2', 14),
+        ('Team 3', 15);
+
+exec [dbo].SendBeLeaderRequest 2, 1
+exec [dbo].SendBeLeaderRequest 13, 2
+exec [dbo].SendBeLeaderRequest 14, 3
+exec [dbo].SendBeLeaderRequest 15, 4
+
+exec [dbo].ApproveRequest 1
+exec [dbo].ApproveRequest 2
+exec [dbo].DeclineRequest 3
+
+exec [dbo].SendJoinTeamRequest 3, 1
+exec [dbo].SendJoinTeamRequest 4, 2
+exec [dbo].SendJoinTeamRequest 5, 2
+exec [dbo].SendJoinTeamRequest 6, 2
+exec [dbo].SendJoinTeamRequest 7, 3
+exec [dbo].SendJoinTeamRequest 8, 3
+exec [dbo].SendJoinTeamRequest 9, 3
+exec [dbo].SendJoinTeamRequest 10, 4
+exec [dbo].SendJoinTeamRequest 11, 4
+exec [dbo].SendJoinTeamRequest 12, 4
+
+exec [dbo].ApproveRequest 5
+exec [dbo].ApproveRequest 6
+exec [dbo].DeclineRequest 7
+
+INSERT INTO [dbo].[Features](Name, [Description], [Version], TeamId)
+    VALUES
+        ('User system', 'User should be able to login. Add roles system. Also add default admin', '1.0', 1),
+        ('Teams system', 'Teams have name, leader and members.', '2.0', 1),
+        ('Features system', 'Features are created by team leaders. They have name, description and version.', '3.0', 1),
+        ('Issues system', 'Isses are created by teams members as a part of some feature. They have name, reporter, assignee, description, version, type, priority, status, resolution, create time and deadline.', '4.0', 1),
+        ('Interaction system', 'Implement user interactions via requests and notifications', '5.0', 1),
+        ('Feature 1', NULL, '1.0', 2),
+        ('Feature 2', NULL, '2.0', 2),
+        ('Feature 3', NULL, '3.0', 2),
+        ('Feature 1', NULL, '1.0', 3),
+        ('Feature 2', NULL, '2.0', 3),
+        ('Feature 3', NULL, '3.0', 3),
+        ('Feature 1', NULL, '1.0', 4),
+        ('Feature 2', NULL, '2.0', 4),
+        ('Feature 3', NULL, '3.0', 4);
+
+INSERT INTO [dbo].[Issues](Name, [Description], [Version], TypeId, PriorityId, StatusId, ResolutionId, CreatedDateUtc, DeadlineDateUtc, FeatureId, ReporterId, AssigneeId)
+    VALUES
+        ('Issue 1', NULL, '0.1', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 6, 4, 5),
+        ('Issue 2', NULL, '0.2', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 6, 5, 4),
+        ('Issue 3', NULL, '0.3', 2, 2, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 6, 6, 4),
+        ('Issue 4', NULL, '0.4', 3, 5, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 6, 4, 6),
+        ('Issue 5', NULL, '0.5', 4, 4, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 6, 5, 6),
+
+        ('Issue 1', NULL, '1.1', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 7, 4, 5),
+        ('Issue 2', NULL, '1.2', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 7, 5, 4),
+        ('Issue 3', NULL, '1.3', 2, 2, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 7, 6, 4),
+        ('Issue 4', NULL, '1.4', 3, 5, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 7, 4, 6),
+        ('Issue 5', NULL, '1.5', 4, 4, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 7, 5, 6),
+
+        ('Issue 1', NULL, '2.1', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 8, 4, 5),
+        ('Issue 2', NULL, '2.2', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 8, 5, 4),
+        ('Issue 3', NULL, '2.3', 2, 2, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 8, 6, 4),
+        ('Issue 4', NULL, '2.4', 3, 5, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 8, 4, 6),
+        ('Issue 5', NULL, '2.5', 4, 4, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 8, 5, 6),
+
+        ('Issue 1', NULL, '0.1', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 9, 7, 8),
+        ('Issue 2', NULL, '0.2', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 9, 8, 7),
+        ('Issue 3', NULL, '0.3', 2, 2, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 9, 9, 7),
+        ('Issue 4', NULL, '0.4', 3, 5, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 9, 7, 9),
+        ('Issue 5', NULL, '0.5', 4, 4, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 9, 8, 9),
+
+        ('Issue 1', NULL, '1.1', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 10, 7, 8),
+        ('Issue 2', NULL, '1.2', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 10, 8, 7),
+        ('Issue 3', NULL, '1.3', 2, 2, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 10, 9, 7),
+        ('Issue 4', NULL, '1.4', 3, 5, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 10, 7, 9),
+        ('Issue 5', NULL, '1.5', 4, 4, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 10, 8, 9),
+
+        ('Issue 1', NULL, '2.1', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 11, 7, 8),
+        ('Issue 2', NULL, '2.2', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 11, 8, 7),
+        ('Issue 3', NULL, '2.3', 2, 2, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 11, 9, 7),
+        ('Issue 4', NULL, '2.4', 3, 5, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 11, 7, 9),
+        ('Issue 5', NULL, '2.5', 4, 4, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 11, 8, 9),
+
+        ('Issue 1', NULL, '0.1', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 12, 10, 11),
+        ('Issue 2', NULL, '0.2', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 12, 11, 10),
+        ('Issue 3', NULL, '0.3', 2, 2, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 12, 12, 10),
+        ('Issue 4', NULL, '0.4', 3, 5, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 12, 10, 12),
+        ('Issue 5', NULL, '0.5', 4, 4, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 12, 11, 12),
+
+        ('Issue 1', NULL, '1.1', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 13, 10, 11),
+        ('Issue 2', NULL, '1.2', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 13, 11, 10),
+        ('Issue 3', NULL, '1.3', 2, 2, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 13, 12, 10),
+        ('Issue 4', NULL, '1.4', 3, 5, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 13, 10, 12),
+        ('Issue 5', NULL, '1.5', 4, 4, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 13, 11, 12),
+
+        ('Issue 1', NULL, '2.1', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 14, 10, 11),
+        ('Issue 2', NULL, '2.2', 1, 3, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 14, 11, 10),
+        ('Issue 3', NULL, '2.3', 2, 2, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 14, 12, 10),
+        ('Issue 4', NULL, '2.4', 3, 5, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 14, 10, 12),
+        ('Issue 5', NULL, '2.5', 4, 4, 1, 1, CAST(GETUTCDATE() AS datetimeoffset), NULL, 14, 11, 12);
